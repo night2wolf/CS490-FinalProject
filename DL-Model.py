3c37170657e1b6be2173bd7c3c2c8c8a5305ec90
Night2wolf <32415142+night2wolf@users.noreply.github.com>
2020-04-08 11:51:57 -0500

    Updated Model

--- DL-Model.py
+++ DL-Model.py
@@ -12,8 +12,20 @@ from keras import backend as K
 
 class DLModel:
   @staticmethod
-  def BuildModel(Learning_Rate, Classes):
+  def BuildModel(Learning_Rate, Classes,Pic_Height,Pic_Width):
+    pic_shape = (Pic_Height,Pic_Width)
     model = Sequential()
-    
-
+    # Input Layer
+    model.add(Dense(512,activation='relu',inputshape=pic_shape))
+    # First Hidden Layer
+    model.add(Dense(512,activation='sigmoid'))
+    #Second Hidden Layer convolution layer with Gaussian Noise
+    model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
+    model.add(Activation('relu'))
+    model.add(GaussianNoise(0.1))
+    # Third hidden layer, convolution layer with dropout
+    model.add(Conv2D(64,(3,3),padding='same',activation='relu'))
+    model.add(Dropout(0.2))
+    #Ouput Layer
+    model.add(Dense(Classes,activation='softmax'))
     return model
